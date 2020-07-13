355c0736f83afe45c8c059bad18ee5ba4f4e9c2c
mstrasser22 <strasser_maria@outlook.com>
2020-07-13 19:17:51 +0200

    newsletter function

--- themes/liva-hugo/assets/js/script.js
+++ themes/liva-hugo/assets/js/script.js
@@ -32,5 +32,26 @@
 
     // venobox initialize
     $('.venobox').venobox();
+	
+	// newsletter
+	$('#newsletter').on('click', function() {
+		const emaildata = $('#newsletterEmail').val();
+		const url = 'https://newsletterfuncapp.azurewebsites.net/api/AddNewsletterEmail?code=OQR48lOOdv2qb7jJqOK1AVP6DwXzoqpeAn7gX32FYEIwq4bj05IPQA==';
+		//$.post(url, { email: emaildata });
+		
+		//$('#newsletterSubscripted').removeAttr('hidden');
+		$.ajax(url, {
+			data: JSON.stringify({email: $('#newsletterEmail').val() }),
+			type: "POST",
+			contentType: "application/json; charset=UTF-8"
+		});
+   
+		//$.post(url,   // url
+			//{ email: emaildata }, // data to be submit
+			   //function(data, status, xhr) {   // success callback function
+                //alert('status: ' + status + ', data: ' + data.responseData);
+            //},
+       //'json'); // response data format
+      })
 
   })(jQuery);
